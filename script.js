f411dfa75bfbcf0ae66511dddd70c1e0d5d5625e
Steph <stephuzoka@gmail.com>
2026-03-17 18:34:39 +0000

    Add hover effect to grid squares using mouseenter event

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-const container = document.querySelector("#container")
+const container = document.querySelector("#container");
 
 function createGrid(size) {
     for (let i = 0; i < size * size; i++){
-        const square = document.createElement("div")
-        square.classList.add("square")
-        container.appendChild(square)
-    }
-}
+        const square = document.createElement("div");
+        square.classList.add("square");
+        container.appendChild(square);
+
+        square.addEventListener("mouseenter", ()=>{
+            square.style.backgroundColor = "black";
+        });
+    };
+};
 createGrid(16);
