ee26c77581b9b88f60e3e1affe9338b0a3b05111
Steph <stephuzoka@gmail.com>
2026-03-17 19:40:51 +0000

    Fix square sizing by adding px units to width and height

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
 const container = document.querySelector("#container");
 
 function createGrid(size) {
+    container.innerHTML = "";
+    const squareSize = 960 / size;
+
     for (let i = 0; i < size * size; i++){
         const square = document.createElement("div");
         square.classList.add("square");
         container.appendChild(square);
 
-        square.addEventListener("mouseenter", ()=>{
+        square.style.width = `${squareSize}px`;
+        square.style.height = `${squareSize}px`;
+
+        square.addEventListener("mouseenter", () => {
             square.style.backgroundColor = "black";
         });
     };
 };
 createGrid(16);
+const resizeBtn = document.querySelector("#resizeBtn");
+resizeBtn.addEventListener("click", () => {
+      let size = prompt("Enter number of squares per side (max 100):");
+      if (size > 100) {
+        alert("Maximum is 100!");
+        return;
+      }
+      
+      createGrid(size);
+});
+
